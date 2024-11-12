5ca86806ea4e9c0a33452c564c5282063cf40b55
Dilean-Shadai <dilean.garcia.ing@gmail.com>
2024-11-12 17:08:16 -0600

    FEATURE: cambios funcionales

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,10 @@
 //rutas para el inicio de sesión 
 const { Router } = require('express');
-const validarToken = require('../utils/token');
+const valarToken = require('../utils/token');
 const router = Router();
 
 // Ruta para la vista de login
-router.get("/login", validarToken, (req, res) => {
+router.get("/login", valarToken, (req, res) => {
   if (res.locals.usuario.nombre !== '') {
     console.log("Usuario ya autenticado, redirecciona a inicio");
     res.redirect('/panel'); // Redirige a panel si el usuario ya está autenticado
@@ -14,35 +14,32 @@ router.get("/login", validarToken, (req, res) => {
 });
 
 // Ruta POST para manejar el inicio de sesión
-router.post("/login", validarToken, async (req, res) => {
-  const { id, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { usuario, pass } = req.body;
   try {
       const response = await fetch('http://localhost:3000/api/auth', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ id, password })
+          body: JSON.stringify({ usuario, pass })
       });
       if (!response.ok) {
           const errorData = await response.json();
           throw new Error(errorData.error || 'Error de autenticación');
       }
       const data = await response.json();
-      const token = data.token;
-      res.cookie('token', token, { httpOnly: true, secure: false });
       res.redirect('/panel'); // Redirige al panel después de autenticarse
 
   } catch (err) {
       console.error('Error al iniciar sesión:', err);
-      res.render('login', { mensaje: "Error de servidor", mostrarHeader: false }); // No mostrar header en caso de error
+      res.render('login', { mensaje: "Error de servor", mostrarHeader: false }); // No mostrar header en caso de error
   }
 });
 
 // Ruta para cerrar sesión
 router.get('/logout', (req, res) => {
-  res.clearCookie('token');
-  res.redirect('/login');
+  res.redirect('/');
 });
 
 module.exports = router;
